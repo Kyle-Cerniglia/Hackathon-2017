a201497f02f5ec399857ee9d8905862d4cc062ff
Netan-MalDoran <kylecerniglia@email.arizona.edu>
2016-12-29 16:28:20 -0700

    Add files via upload

---
+++ mapper_V1.0/mapper_V1.0.ino
@@ -0,0 +1,112 @@
+#include <SD.h>
+#include <SPI.h>
+#include <Servo.h>
+
+Servo servo1;//Straight is 70', 0-140 0 = right 140 = left
+File rangedata;
+
+int delayus = 0;
+int range = 0;
+int range_s = 0;
+int count = 0;
+int range_a[142];
+int degree = 0;
+int x = 0;
+int y = 0;
+
+void setup() {
+  
+  pinMode(0, INPUT);    //EncoderA
+  pinMode(1, INPUT);    //EncoderB
+  pinMode(4, OUTPUT);   //D1
+  pinMode(5, OUTPUT);   //S1
+  pinMode(6, OUTPUT);   //S2
+  pinMode(7, OUTPUT);   //D2
+  pinMode(8, OUTPUT);   //SDCS
+  pinMode(9, OUTPUT);   //MAGCS
+  pinMode(A0, OUTPUT);  //SOUT
+  pinMode(A1, INPUT);   //SIN
+  //pinMode(10, OUTPUT);  //Servo
+  pinMode(13, OUTPUT);  //LED
+
+  digitalWrite(13, LOW);
+  
+  servo1.attach(10);
+  SD.begin(8);
+  Serial.begin(9600);
+
+
+  
+}
+
+void loop() {
+
+  //Rangefinding
+  degree = 0;
+  
+  while(degree < 142){
+    servo1.write(degree);
+    delay(10);
+   
+  
+    while(count < 50){
+      digitalWrite(A0, LOW);
+      delayMicroseconds(2);
+      digitalWrite(A0, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(A0, LOW);
+      delayus = pulseIn(A1, HIGH, 30000);
+      
+      range = delayus;
+      range /= 59;
+      range_s += range;
+      count++;
+    }
+    
+    range = range_s / 50;
+    range_a[degree] = range;
+
+    degree++;
+    
+    range_s = 0;
+    count = 0;
+    range = 0;
+    
+  }
+  degree = 0;
+  
+/*
+  degree = 141;
+  while(degree >= 0){
+    Serial.print(range_a[degree]);
+    Serial.print(" ");
+    degree--;
+  }
+  degree = 0;
+  Serial.println();
+*/
+
+//SD Card
+  rangedata = SD.open("rangdata.txt", FILE_WRITE);
+  if(rangedata){
+    degree = 0;
+    while(degree < 142){
+      rangedata.print(x);
+      rangedata.print(":");
+      rangedata.print(y);
+      rangedata.print(":");
+      rangedata.print(degree);
+      rangedata.print(":");
+      rangedata.print(range_a[degree]);
+      rangedata.println(";");
+      degree++;
+    }
+    degree = 0;
+    rangedata.close();
+  }
+  else{
+    digitalWrite(13, HIGH);
+    while(1){}
+  }
+
+}
---
+++ photoplotter/main.cpp
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdio>
+#include <math.h>
+
+#define RAD 0.01745
+
+using namespace std;
+
+FILE* read = NULL;
+FILE* write = NULL;
+
+int x = 0;
+int y = 0;
+int degree = 0;
+int range = 0;
+int range_a[4][142];//x, y, degree, range
+int photogrid[300][200];
+int i = 0;
+
+int main()
+{
+    //File open
+    read = fopen("rangdata.txt", "r");
+    if(read == NULL){
+        printf("Read error");
+        return 0;
+    }
+
+    //File read
+    i = 0;
+    while(i < 142){
+        fscanf(read, "%d", &range_a[0][i]);
+        fscanf(read, ":%d", &range_a[1][i]);
+        fscanf(read, ":%d", &range_a[2][i]);
+        fscanf(read, ":%d", &range_a[3][i]);
+        fscanf(read, ";\n");
+        i++;
+    }
+    i = 0;
+    fclose(read);
+
+    //Point generation
+    while(i < 71){
+        range_a[2][i] = 71 - range_a[2][i];
+        x = range_a[3][i] * sin(RAD * range_a[2][i]);
+        y = range_a[3][i] * cos(RAD * range_a[2][i]);
+        photogrid[x + 150][200 - y] = 1;
+        i++;
+    }
+    i = 0;
+
+    photogrid[150][200 - range_a[2][71]] = 1;
+
+    i = 72;
+    while(i < 142){
+        range_a[2][i] = range_a[2][i] - 71;
+        x = range_a[3][i] * sin(RAD * range_a[2][i]);
+        y = range_a[3][i] * cos(RAD * range_a[2][i]);
+        photogrid[150 - x][200 - y] = 1;
+        i++;
+    }
+    i = 0;
+/*
+    //Photo print
+    x = 0;
+    y = 0;
+    while(y < 300){
+        while(x < 300){
+            if(photogrid[x][y] == 1){
+                printf(".");
+                i++;
+            }
+            else{
+                printf(" ");
+            }
+            x++;
+        }
+        x = 0;
+        printf("\n");
+        y++;
+    }
+    y = 0;
+*/
+
+    //Photo write
+
+    write = fopen("processed.txt", "w");
+    if(write == NULL){
+        printf("Write error\n");
+        return 0;
+    }
+
+    x = 0;
+    y = 0;
+    while(y < 200){
+        while(x < 300){
+            if(photogrid[x][y] == 1){
+                fprintf(write, "O");
+            }
+            else{
+                fprintf(write, " ");
+            }
+            x++;
+        }
+        x = 0;
+        fprintf(write, "\n");
+        y++;
+    }
+    y = 0;
+    fclose(write);
+    return 0;
+}
