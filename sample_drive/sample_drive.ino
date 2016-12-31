1809442fae48ca7c10a2d7d1d473417eddf3c822
Netan-MalDoran <kylecerniglia@email.arizona.edu>
2016-12-30 22:17:35 -0700

    Add files via upload

---
+++ sample_drive/sample_drive.ino
@@ -0,0 +1,44 @@
+//Drives forwards and backwards then turns left then right
+
+int pin_motor_a_dir = 4;
+int pin_motor_a_spd = 5;
+int pin_motor_b_dir = 7;
+int pin_motor_b_spd = 6;
+
+void setup() {
+  pinMode(pin_motor_a_dir, OUTPUT);
+  pinMode(pin_motor_a_spd, OUTPUT);
+  pinMode(pin_motor_b_dir, OUTPUT);
+  pinMode(pin_motor_b_spd, OUTPUT);
+}
+
+void loop() {
+
+  //Forwards
+  digitalWrite(pin_motor_a_dir, 0);
+  analogWrite(pin_motor_a_spd, 255);
+  digitalWrite(pin_motor_b_dir, 0);
+  analogWrite(pin_motor_b_spd, 255);
+  delay(1000);
+  
+  //Backwards
+  digitalWrite(pin_motor_a_dir, 1);
+  analogWrite(pin_motor_a_spd, 255);
+  digitalWrite(pin_motor_b_dir, 1);
+  analogWrite(pin_motor_b_spd, 255);
+  delay(1000);
+
+  //Left
+  digitalWrite(pin_motor_a_dir, 1);
+  analogWrite(pin_motor_a_spd, 255);
+  digitalWrite(pin_motor_b_dir, 0);
+  analogWrite(pin_motor_b_spd, 255);
+  delay(1000);
+
+  //Right
+  digitalWrite(pin_motor_a_dir, 0);
+  analogWrite(pin_motor_a_spd, 255);
+  digitalWrite(pin_motor_b_dir, 1);
+  analogWrite(pin_motor_b_spd, 255);
+  delay(1000);
+}
